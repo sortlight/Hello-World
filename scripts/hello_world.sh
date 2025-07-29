cebda27a59e37a9737e9e8f924a29697195d0737
Sortsec <149396850+sortlight@users.noreply.github.com>
2025-07-28 20:35:49 -0700

    Update and rename day1_hello_world.sh to hello_world.sh

---
+++ scripts/hello_world.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Day 1: Hello World Bash Script
+
+echo "Hello, Sortlight"
+echo "Today is $(date)"
+echo "You’ve just written your first Bash script. Glory to God!"
+echo "Testing Testing Testing"
